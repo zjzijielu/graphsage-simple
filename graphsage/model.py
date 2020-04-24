d5bc9fcf03891205df4bf90be1214252cc713978
yangji9181 <yangji9181@163.com>
2020-04-24 16:29:35 -0500

    update

--- graphsage/model.py
+++ graphsage/model.py
@@ -405,7 +405,7 @@ def load_pubmed(feature_dim, initializer):
             feat_data[k, 0] = v
     elif initializer == "eigen_decomposition":
         adj_matrix = nx.to_numpy_array(G)
-        w, v = LA.eig(adj_matrix)
+	w, v = LA.eig(adj_matrix)
         indices = np.argsort(w)
         feat_data = np.zeros((num_nodes, feature_dim))
         for i in range(num_nodes):
